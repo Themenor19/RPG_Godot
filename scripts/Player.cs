5b5899151ff8f93367825a769c94c1644752cf1c
Themenor19 <tylerbaum128@gmail.com>
2026-05-11 03:36:34 -0400

    finished animation and y-sorting of current objects

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,11 +3,16 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float Speed = 100.0f;
 	public const float JumpVelocity = -400.0f;
+	public AnimatedSprite2D sprite;
 
 	public override void _Ready()
 	{
+		sprite = GetNode<AnimatedSprite2D>("PlayerSprite");
+		sprite.Play("front_standing_idle");
+		Godot.GD.Print(sprite.Animation);
+
 		
 	}
 	
@@ -20,6 +25,7 @@ public partial class Player : CharacterBody2D
 		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero)
 		{
+			sprite.Play(direction.X < 0 ? "walk_left" : "walk_right");
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
 		}
@@ -27,6 +33,7 @@ public partial class Player : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			sprite.Play("front_standing_idle");
 		}
 
 		Velocity = velocity;
