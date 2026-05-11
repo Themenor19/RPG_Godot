9fc89042a832c66e7b1cd33046ec0ceaa5540ac3
Themenor19 <tylerbaum128@gmail.com>
2026-05-11 01:28:47 -0400

    added new sprites :

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,6 +6,11 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
+	public override void _Ready()
+	{
+		
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
