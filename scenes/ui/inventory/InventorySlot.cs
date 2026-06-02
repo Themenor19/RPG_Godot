ac8c4c8703f335e71e3dd9dae69becb8ac7c4fd5
Themenor19 <tylerbaum128@gmail.com>
2026-06-02 16:44:05 -0400

    inventory stuff

--- scenes/ui/inventory/InventorySlot.cs
+++ scenes/ui/inventory/InventorySlot.cs
@@ -3,28 +3,28 @@ using System;
 
 public partial class InventorySlot : Control
 {
-    private TextureRect _icon;
-    private Label _quantity;
-    private NinePatchRect _detailsPanel;
-    private Label _itemName;
-    private Label _itemType;
-    private Label _itemEffect;
-    private NinePatchRect _usagePanel;
-    
-    public void _on_item_button_mouse_entered()
-    {
-        _icon = GetNode<TextureRect>("ItemPanel/ItemIcon");
-        _quantity = GetNode<Label>("ItemPanel/ItemQuantity");
-        _itemName = GetNode<Label>("00");
-    }
+	private TextureRect _icon;
+	private Label _quantity;
+	private NinePatchRect _detailsPanel;
+	private Label _itemName;
+	private Label _itemType;
+	private Label _itemEffect;
+	private NinePatchRect _usagePanel;
+	
+	public void _on_item_button_mouse_entered()
+	{
+		_icon = GetNode<TextureRect>("ItemPanel/ItemIcon");
+		_quantity = GetNode<Label>("ItemPanel/ItemQuantity");
+		_itemName = GetNode<Label>("00");
+	}
 
-    public void _on_item_button_mouse_exited()
-    {
-        
-    }
+	public void _on_item_button_mouse_exited()
+	{
+		
+	}
 
-    public void _on_item_button_pressed()
-    {
-        
-    }
+	public void _on_item_button_pressed()
+	{
+		
+	}
 }
