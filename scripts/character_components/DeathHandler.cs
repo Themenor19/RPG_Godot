268147ae6d19b905f9d0dcd683a6224367cfca18
Themenor19 <tylerbaum128@gmail.com>
2026-07-21 00:23:21 -0400

    added borders to the dungeon tileset and created test dungeon level

--- scripts/character_components/DeathHandler.cs
+++ scripts/character_components/DeathHandler.cs
@@ -1,28 +1,25 @@
 using Godot;
+using RPG.scripts.globals;
 
 namespace RPG.scripts.character_components;
 
 public partial class DeathHandler : Node2D
 {
 	[Export] public HealthBar HealthBar;
-	[Export] public Node Parent;
+	[Export] public Node2D Parent;
+	
+	private Global _global;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		HealthBar.Dead += Die;
+		_global = Global.Instance;
 	}
 
 	public void Die()
 	{
-		CallDeferred(nameof(KillParent));
-	}
-
-	public void KillParent()
-	{
-		if (Parent != null)
-		{
-			Parent.QueueFree();
-		}
+		Parent.Visible = false;
+		_global.PlayerMoveScenes("uid://bibtx3p5das13");;
 	}
 }
